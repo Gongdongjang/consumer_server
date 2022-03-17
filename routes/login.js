093c30584a46723131e665a0bd809549f228d9f8
Gongdongjang <jeesoo0908@gmail.com>
2022-03-17 16:49:02 +0900

    [버그] 기본 회원가입, 로그인 수정

--- routes/login.js
+++ routes/login.js
@@ -1,39 +1,39 @@
-const db = require("../db.js");
+const pool = require("../db");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.post("/login", function (req, res) {
-  console.log(req.body);
-  var user_id = req.body.user_id;
-  var password = req.body.password;
-  var sql = "SELECT * FROM user WHERE user_id = ?";
+router.post("/login", async (req, res, next) => {
+  const {user_id, password} = req.body;
+  var resultCode = 404;
+  var message = "에러가 발생했습니다";
 
-  db.query(sql, user_id, function (err, result) {
-    var resultCode = 404;
-    var message = "에러가 발생했습니다";
-
-    if (err) {
-      console.log(err);
+  try {
+    //문제 없으면 try문 실행
+    const data = await pool.query("SELECT * FROM user WHERE user_id = ?", [
+      user_id,
+    ]);
+    if (data[0][0] == undefined) {
+      // 계정이 없다면
+      resultCode = 206;
+      message = "존재하지 않는 계정입니다!";
+    } else if (!bcrypt.compareSync(password, data[0][0].password)) {
+      // 비밀번호가 다르다면
+      resultCode = 204;
+      message = "비밀번호가 틀렸습니다!";
     } else {
-      if (result.length === 0) {
-        resultCode = 206;
-        message = "존재하지 않는 계정입니다!";
-      } else if (!bcrypt.compareSync(password, result[0].password)) {
-        resultCode = 204;
-        message = "비밀번호가 틀렸습니다!";
-      } else {
-        resultCode = 200;
-        message = "로그인 성공! " + result[0].user_name + "님 환영합니다!";
-      }
+      // 다른 경우는 없다고 판단하여 성공
+      resultCode = 200;
+      message = "로그인 성공! " + data[0][0].user_name + "님 환영합니다!";
     }
-
-    res.json({
+    return res.json({
       code: resultCode,
       message: message,
       user_id: user_id,
     });
-  });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,41 +1,34 @@
-const db = require("../db.js");
+const pool = require("../db");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.post("/signup", function (req, res) {
-  // console.log(req.body);
-  var user_id = req.body.user_id;
-  var password = req.body.password;
-  var user_name = req.body.user_name;
-  var nickname = user_id;
-  var mobile_no = req.body.mobile_no;
-  var gender = req.body.gender;
-  password = bcrypt.hashSync(password, 10); // sync
-
-  // 삽입을 수행하는 sql문.
-  var sql =
-    "INSERT INTO user (user_id, password, user_name, nickname, mobile_no, gender) VALUES (?, ?, ?, ?, ?, ?)";
-  var params = [user_id, password, user_name, nickname, mobile_no, gender];
-
-  // sql 문의 ?는 두번째 매개변수로 넘겨진 params의 값으로 치환된다.
-  db.query(sql, params, function (err, result) {
-    var resultCode = 404;
-    var message = "에러가 발생했습니다";
-
-    if (err) {
-      console.log(err);
-    } else {
-      resultCode = 200;
-      message = "회원가입에 성공했습니다.";
-    }
-
-    res.json({
+router.post("/signup", async (req, res, next) => {
+  //next 추가하기 전에는 오류났었는데 next 없어도 되는 건지는 모르겠음
+  console.log(req.body);
+  const {user_id, password, user_name, nickname, mobile_no, gender} = req.body;
+  const passwordBy = bcrypt.hashSync(password, 10); // sync
+  var resultCode = 404;
+  var message = "에러가 발생했습니다.";
+  try {
+    //문제 없으면 try문 실행
+    const data = await pool.query(
+      "INSERT INTO user (user_id, password, user_name, nickname, mobile_no, gender) VALUES (?, ?, ?, ?, ?, ?)",
+      //insert에 딕셔너리 형태로 적었더니 오류 -> 리스트 형태가 맞음
+      [user_id, passwordBy, user_name, nickname, mobile_no, gender]
+    );
+    resultCode = 200;
+    message = "회원가입에 성공했습니다!";
+    console.log("회원가입 성공");
+    return res.json({
       code: resultCode,
       message: message,
       user_id: user_id,
     });
-  });
+  } catch (err) {
+    //에러 처리
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
