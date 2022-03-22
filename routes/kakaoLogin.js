cfe5d9d7b7cc13a0b2d7ed67c8d02b9f2d240779
leejw-lu <jiwoojenny@naver.com>
2022-03-23 00:18:12 +0900

    [버그] 카카오 간편 로그인 수정

--- routes/kakaoLogin.js
+++ routes/kakaoLogin.js
@@ -1,52 +1,47 @@
-const db = require("../db.js");
+const pool = require("../db");
 const express = require("express");
 const router = express.Router();
 
-router.post("/kakaoLogin", function (req, res) {
+router.post("/kakaoLogin", async (req, res, next) => {
   console.log("카카오 로그인 " + req.body.user_id);
-  //console.log("카카오 로그인 " + req.body.sns_type);
-  var user_id = req.body.user_id;
-  var user_name = req.body.user_name;
-  var nickname = req.body.nickname;
-  var sns_type=req.body.sns_type;
-  var refresh_token=req.body.refresh_token;
-  var gender= req.body.gender;
-
+  const {user_id, user_name, nickname, sns_type, refresh_token, gender}=req.body;
   var sql = "INSERT INTO user (user_id, user_name, nickname, sns_type, refresh_token, gender) VALUES (?, ?, ?, ?, ?, ?)";
   var param=[user_id, user_name, nickname,sns_type,refresh_token, gender];
 
-  db.query(
-    "SELECT * FROM user WHERE user_id = ?", [user_id], function (err, result) {
-      var resultCode = 404;
-      var message = "에러가 발생했습니다";
-      console.log(result);
+  console.log(req.body);
+  var resultCode = 404;
+  var message = "에러가 발생했습니다.";
 
-      if (result == null) {
-        db.query(sql, param, function (error, output) {
-          if (error) {
-            console.error(error);
-          } else {
-            resultCode = 200;
-            message =
-            "카카오 계정 회원가입 성공! " +
-            result[0].user_name +
-            "님 환영합니다!";
-          }
-        });
-      } else {
-        //console.log("여긴login_result"+result);
-        resultCode = 200;
-        message = result[0].user_name + "님 환영합니다!";
-      }
-
-      res.json({
-        code: resultCode,
-        message: message,
-        user_id: user_id,
-      });
+  try{
+    var data= await pool.query(
+      "SELECT * FROM user WHERE user_id = ?", [user_id],
+    );
+    
+    if(data[0][0]==undefined){
+      console.log("23행- data[0][0] undefined");
+      data= await pool.query(
+        sql,
+        param
+      );
+      //console.log("28행", data[0][0].user_name);
+      resultCode = 200;
+      message = "카카오 계정 회원가입 성공! " + data[0][0].user_name + "님 환영합니다!";
     }
-  );
+    else{
+      //console.log("33행 여긴login_result"+data);
+      resultCode = 200;
+      message = data[0][0].user_name + "님 환영합니다!";
+    }
+    res.json({
+      code: resultCode,
+      message: message,
+      user_id: user_id,
+    });
+
+  } catch(err){
+    console.log("catch-error부분");
+    return res.status(500).json(err);
+  }
 });
 
-module.exports = router;
-
+  module.exports = router;
