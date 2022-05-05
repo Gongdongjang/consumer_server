359a5f5a945fdfc303c6a9b899b0bc84436543e8
Gongdongjang <jeesoo0908@gmail.com>
2022-05-05 22:47:32 +0900

    [기능추가] 스토어 및 제품명 띄우기

--- routes/storeView.js
+++ routes/storeView.js
@@ -8,10 +8,16 @@ router.get("/storeView", async (req, res, next) => {
 
   try {
     //문제 없으면 try문 실행
-    const data = await pool.query("SELECT md_id, store_id FROM pickup");
+    const data = await pool.query(
+      "SELECT pu_start, pu_end, md_id, store_id FROM pickup"
+    );
+
     let st_arr = new Array();
     let md_arr = new Array();
-    var count = await pool.query("SELECT COUNT(*) FROM pickup");
+    let pu_start = new Array();
+    let pu_end = new Array();
+
+    let count = await pool.query("SELECT COUNT(*) FROM pickup");
     count = count[0][0]["COUNT(*)"];
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
@@ -20,11 +26,10 @@ router.get("/storeView", async (req, res, next) => {
         [data[0][i].store_id]
       );
       st_arr[i] = store_name[0][0].store_name;
+      // pu_start[i] = moment(data[0][i].pu_start).format("YYYY-MM-DD");
+      // pu_end[i] = moment(data[0][i].pu_end).format("YYYY-MM-DD");
       connection.release();
     }
-    // console.log(st_arr);
-    // let jsonSt_name = JSON.stringify(st_obj);
-    // console.log(jsonSt_name);
 
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
@@ -34,17 +39,16 @@ router.get("/storeView", async (req, res, next) => {
       md_arr[i] = md_name[0][0].md_name;
       connection.release();
     }
-    // console.log(md_arr);
 
-    // store = data[0];
+    // console.log(pu_start);
+    // console.log(pu_end);
 
     return res.json({
       code: resultCode,
       message: message,
       count: count,
-      // store: store,
-      md_obj: md_obj,
-      st_obj: st_obj,
+      md_arr: md_arr,
+      st_arr: st_arr,
     });
   } catch (err) {
     return res.status(500).json(err);
