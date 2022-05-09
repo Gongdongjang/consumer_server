7201081d1d4388848b7496c9234b54de7c4ce620
Gongdongjang <jeesoo0908@gmail.com>
2022-05-10 01:52:36 +0900

    [기능추가] 픽업 가능한 스토어 정보 보기(가격 제외)

--- routes/storeView.js
+++ routes/storeView.js
@@ -16,6 +16,7 @@ router.get("/storeView", async (req, res, next) => {
     let md_arr = new Array();
     let pu_start = new Array();
     let pu_end = new Array();
+    let store = new Array();
 
     let count = await pool.query("SELECT COUNT(*) FROM pickup");
     count = count[0][0]["COUNT(*)"];
@@ -26,22 +27,26 @@ router.get("/storeView", async (req, res, next) => {
         [data[0][i].store_id]
       );
       st_arr[i] = store_name[0][0].store_name;
-      // pu_start[i] = moment(data[0][i].pu_start).format("YYYY-MM-DD");
-      // pu_end[i] = moment(data[0][i].pu_end).format("YYYY-MM-DD");
       connection.release();
     }
 
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
-      md_name = await pool.query("SELECT md_name FROM md where md_id = ?", [
-        data[0][i].md_id,
-      ]);
+      md_name = await pool.query(
+        "SELECT md_name, md_start, md_end FROM md where md_id = ?",
+        [data[0][i].md_id]
+      );
       md_arr[i] = md_name[0][0].md_name;
+      pu_start[i] = new Date(md_name[0][0].md_start).toLocaleDateString();
+      pu_end[i] = new Date(md_name[0][0].md_end).toLocaleDateString();
       connection.release();
     }
 
-    // console.log(pu_start);
-    // console.log(pu_end);
+    // for (let i = 0; i < count; i++) {
+    //   store[i] = [st_arr[i], md_arr[i], pu_start[i], pu_end[i]];
+    // }
+
+    // console.log(store);
 
     return res.json({
       code: resultCode,
@@ -49,6 +54,9 @@ router.get("/storeView", async (req, res, next) => {
       count: count,
       md_arr: md_arr,
       st_arr: st_arr,
+      pu_start: pu_start,
+      pu_end: pu_end,
+      // store: store,
     });
   } catch (err) {
     return res.status(500).json(err);
