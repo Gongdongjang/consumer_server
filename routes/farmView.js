07b2acb22bd1cd971edb17ba0f8226c38bf7161d
leejw-lu <jiwoojenny@naver.com>
2022-05-14 17:08:09 +0900

    [기능추가] 전체입점농가 -세부페이지 추가정보 보내기

--- routes/farmView.js
+++ routes/farmView.js
@@ -17,6 +17,9 @@ router.get("/farmView", async (req, res, next) => {
     let farm_mainItem = new Array();
     let farm_info = new Array();
     let farm_loc = new Array();
+    let farm_lat=new Array();
+    let farm_long=new Array();
+    let farm_hours=new Array();
 
     let count = await pool.query("SELECT COUNT(*) FROM farm");
     count = count[0][0]["COUNT(*)"];
@@ -24,7 +27,7 @@ router.get("/farmView", async (req, res, next) => {
 
     for (let i=0; i< count; i++) { //재할당이니까 const가 아닌 let이다..!!
       console.log(i);
-      let data = await pool.query("SELECT farm_name, farm_info, farm_mainItem, farm_loc FROM farm");
+      let data = await pool.query("SELECT farm_name, farm_info, farm_mainItem, farm_loc, farm_lat, farm_long, farm_hours FROM farm");
   
       //console.log("data[0][i] 번째");
 
@@ -32,13 +35,19 @@ router.get("/farmView", async (req, res, next) => {
       farm_mainItem[i]=data[0][i].farm_mainItem;
       farm_info[i]=data[0][i].farm_info;
       farm_loc[i]=data[0][i].farm_loc;
-
+      farm_lat[i]=data[0][i].farm_lat;
+      farm_long[i]=data[0][i].farm_long;
+      farm_hours[i]=data[0][i].farm_hours;
     }
-    console.log("63행");
+
+    console.log("farm_details 정보들");
     console.log(farm_arr);
     console.log(farm_mainItem);
     console.log(farm_info);
     console.log(farm_loc);
+    console.log(farm_lat);
+    console.log(farm_long);
+    console.log(farm_hours);
 
 
     return res.json({
@@ -49,6 +58,9 @@ router.get("/farmView", async (req, res, next) => {
       farm_mainItem: farm_mainItem,
       farm_info: farm_info,
       farm_loc: farm_loc,
+      farm_lat: farm_lat,
+      farm_long: farm_long,
+      farm_hours: farm_hours,
     });
   } catch (err) {
     return res.status(500).json(err);
