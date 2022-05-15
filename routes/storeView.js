dbae5a16f3188dab1079aaef1663dff850a38831
leejw-lu <jiwoojenny@naver.com>
2022-05-15 19:39:02 +0900

    [기능추가] 전체픽업스토어-세부페이지 추가정보 보내기

--- routes/storeView.js
+++ routes/storeView.js
@@ -16,17 +16,33 @@ router.get("/storeView", async (req, res, next) => {
     let md_arr = new Array();
     let pu_start = new Array();
     let pu_end = new Array();
-    let store = new Array();
+    //let store = new Array();
+
+    //세부페이지에 쓰일 store테이블 속성들
+    let store_info = new Array();
+    let store_hours = new Array();
+    let store_restDays = new Array();
+    let store_loc = new Array();
+    let store_lat = new Array();
+    let store_long= new Array();
 
     let count = await pool.query("SELECT COUNT(*) FROM pickup");
     count = count[0][0]["COUNT(*)"];
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
       store_name = await pool.query(
-        "SELECT store_name FROM store where store_id = ?",
+        "SELECT store_name,store_info,store_hours,store_restDays,store_loc,store_lat,store_long FROM store where store_id = ?",
         [data[0][i].store_id]
       );
       st_arr[i] = store_name[0][0].store_name;
+      //세부
+      store_info[i]=store_name[0][0].store_info;
+      store_hours[i]=store_name[0][0].store_hours;
+      store_restDays[i]=store_name[0][0].store_restDays;
+      store_loc[i]=store_name[0][0].store_loc;
+      store_lat[i]=store_name[0][0].store_lat;
+      store_long[i]=store_name[0][0].store_long;
+      
       connection.release();
     }
 
@@ -42,21 +58,30 @@ router.get("/storeView", async (req, res, next) => {
       connection.release();
     }
 
-    // for (let i = 0; i < count; i++) {
-    //   store[i] = [st_arr[i], md_arr[i], pu_start[i], pu_end[i]];
-    // }
+    console.log("66행");
+    console.log(store_info);
+    console.log(store_hours);
+    console.log(store_restDays);
+    console.log(store_loc);
 
-    // console.log(store);
 
     return res.json({
       code: resultCode,
       message: message,
       count: count,
+      //store_count: store_count,
       md_arr: md_arr,
       st_arr: st_arr,
       pu_start: pu_start,
       pu_end: pu_end,
-      // store: store,
+      // 세부
+      store_info:store_info,
+      store_hours:store_hours,
+      store_restDays: store_restDays,
+      store_loc: store_loc,
+      store_lat: store_lat,
+      store_long: store_long,
+
     });
   } catch (err) {
     return res.status(500).json(err);
