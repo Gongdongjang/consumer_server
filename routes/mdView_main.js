7862a5147ed7457b367bfb1b097f6ef06a8e3dac
Gongdongjang <jeesoo0908@gmail.com>
2022-05-21 16:33:29 +0900

    [기능추가] 제품리스트(메인) 띄우기

--- routes/mdView_main.js
+++ routes/mdView_main.js
@@ -8,42 +8,75 @@ router.get("/mdView_main", async (req, res, next) => {
 
   try {
     //문제 없으면 try문 실행
-    const data = await pool.query("SELECT md_id, store_id FROM pickup");
+    const data = await pool.query(
+      "SELECT pu_start, pu_end, md_id, store_id FROM pickup"
+    );
 
-    let st_name = new Array();
+    let pu_start = new Array();
+    let pu_end = new Array();
+    let store_name = new Array();
     let md_name = new Array();
+    let pay_schedule = new Array();
+    let farm_id = new Array();
+    let farm_name = new Array();
+    // console.log("22");
 
-    let count = await pool.query("SELECT COUNT(*) FROM pickup");
+    let count = await pool.query("SELECT COUNT(*) FROM md"); //md나 pickup이나 행 개수가 같아야 되는 것 같음
     count = count[0][0]["COUNT(*)"];
 
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
-      store_name = await pool.query(
+      let st_name = await pool.query(
         "SELECT store_name FROM store WHERE store_id = ?",
         [data[0][i].store_id]
       );
-      st_name[i] = store_name[0][0].store_name;
+      pu_start[i] = new Date(data[0][i].pu_start).toLocaleDateString();
+      pu_end[i] = new Date(data[0][i].pu_end).toLocaleDateString();
+      store_name[i] = st_name[0][0].store_name;
       connection.release();
     }
+
+    // console.log("39");
 
     for (let i = 0; i < count; i++) {
       let connection = await pool.getConnection(async (conn) => conn);
-      md_n = await pool.query("SELECT md_name FROM md WHERE md_id = ?", [
-        data[0][i].md_id,
-      ]);
+      md_n = await pool.query(
+        "SELECT md_name, farm_id, pay_schedule FROM md, payment WHERE md.md_id = ?",
+        [data[0][i].md_id]
+      );
       md_name[i] = md_n[0][0].md_name;
+      pay_schedule[i] = new Date(md_n[0][i].pay_schedule).toLocaleDateString();
+      farm_id[i] = md_n[0][0].farm_id;
       connection.release();
     }
 
+    // console.log("53");
+
+    for (let i = 0; i < count; i++) {
+      let connection = await pool.getConnection(async (conn) => conn);
+      // console.log(farm_id);
+
+      let frm_name = await pool.query(
+        `SELECT farm_name FROM farm where farm_id = ?`,
+        [farm_id[i]]
+      );
+      // console.log(farm_id);
+      farm_name[i] = frm_name[0][0].farm_name;
+      connection.release();
+    }
     // console.log(st_name);
-    // console.log(md_name);
+    // console.log(count);
 
     return res.json({
       code: resultCode,
       message: message,
-      st_name: st_name,
+      store_name: store_name,
       md_name: md_name,
       count: count,
+      pu_start: pu_start,
+      pu_end: pu_end,
+      pay_schedule: pay_schedule,
+      farm_name: farm_name,
     });
   } catch (err) {
     return res.status(500).json(err);
