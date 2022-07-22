823f15a1eb0d3337d4553c3ba74f1a8f02203c1b
Suanna01 <yangnony01@gmail.com>
2022-07-22 23:41:30 +0900

    [기능수정] 날짜 형식 개선

--- routes/mdView_main.js
+++ routes/mdView_main.js
@@ -7,14 +7,31 @@ router.get("/mdView_main", async (req, res, next) => {
   var message = "에러가 발생했습니다.";
 
   try {
+    //md, payment, pickup, store, farm
+    const [md_result] = await pool.execute("select md.md_id, md_name, pu_start, pu_end, pay_schedule, farm_name, store_name from md join farm on md.farm_id=farm.farm_id join payment on md.md_id=payment.md_id join pickup on md.md_id=pickup.md_id join store on pickup.store_id=store.store_id ORDER BY md.md_id desc");
+    console.log(md_result);
+    
+    let count = await pool.query("SELECT COUNT(*) FROM md");
+    count = count[0][0]["COUNT(*)"];
 
-    const [result] = await pool.execute("select md_name, pu_start, pu_end, pay_schedule, farm_name, store_name from md join farm on md.farm_id=farm.farm_id join payment on md.md_id=payment.md_id join pickup on md.md_id=pickup.md_id join store on pickup.store_id=store.store_id ORDER BY md.md_id desc");
+    let pay_schedule = new Array();
+    let pu_start = new Array();
+    let pu_end = new Array();
 
-    console.log(result);
+    for (let i = 0; i < count; i++){
+      pay_schedule[i] = new Date(md_result[i].pay_schedule).toLocaleDateString();
+      pu_start[i] = new Date(md_result[i].pu_start).toLocaleDateString();
+      pu_end[i] = new Date(md_result[i].pu_end).toLocaleDateString();
+    }
 
     return res.json({
-        result:result,
-        // count: count,
+      code: resultCode,
+      message: message,
+      count: count,
+      md_result:md_result,
+      pay_schedule:pay_schedule,
+      pu_start:pu_start,
+      pu_end:pu_end,
     });
   } catch (err) {
     console.error(err);
