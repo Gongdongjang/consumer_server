dab07928efad57651f480e6fada429b5f9ee7be4
leejw-lu <jiwoojenny@naver.com>
2022-09-14 23:26:50 +0900

    [기능수정] lat, long 속성 삭제 및 loc 추가

--- routes/storeView.js
+++ routes/storeView.js
@@ -9,7 +9,7 @@ router.get("/storeView", async (req, res, next) => {
   try {
     //문제 없으면 try문 실행
     const [store_result] = await pool.execute(
-      `SELECT store_id, store_name, store_info, store_hours, store_restDays, store_lat, store_long FROM store`
+      `SELECT store_id, store_name, store_info, store_hours, store_loc FROM store`
     );
 
     let count = await pool.query("SELECT COUNT(*) FROM store");
