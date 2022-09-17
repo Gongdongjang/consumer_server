4bd4856b7f1c923970211619c1f3c7d0ab7ea5b5
leejw-lu <jiwoojenny@naver.com>
2022-09-18 00:02:03 +0900

    [기능수정] 주소정보 개수 오류 수정

--- routes/get_address.js
+++ routes/get_address.js
@@ -3,32 +3,34 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
-
-  const userid=req.body.id;
+  const userid = req.body.id;
 
   let resultCode = 404;
   let message = "에러가 발생했습니다.";
 
   try {
-    const u_data = await pool.query("SELECT user_no FROM user WHERE user_id=? ", [userid]);
-    userno=u_data[0][0].user_no;
+    const u_data = await pool.query(
+      "SELECT user_no FROM user WHERE user_id=? ",
+      [userid]
+    );
+    userno = u_data[0][0].user_no;
 
     //문제 없으면 try문 실행
     const [address_result] = await pool.execute(
       `SELECT loc1, loc2, loc3 FROM address_user WHERE userno=${userno}`
     );
 
-    let address_count=3;
+    let address_count = 3;
 
-    if(address_result.loc3 == null ){
-        address_count--;    //address_count=2
-    } else if (address_result.loc2==null){
-        address_count--;    //address_count=1
-    } else if (address_result.loc1==null){
-        address_count--;    //address_count==0
+    if (address_result[0].loc3 == null) {
+      address_count--; //address_count=2
     }
-
-    //console.log(address_count);
+    if (address_result[0].loc2 == null) {
+      address_count--; //address_count=1
+    }
+    if (address_result[0].loc1 == null) {
+      address_count--; //address_count==0
+    }
 
     resultCode = 200;
     message = "주소정보 얻기 성공";
@@ -37,7 +39,7 @@ router.post("/", async (req, res, next) => {
       code: resultCode,
       message: message,
       address_result: address_result,
-      address_count: address_count
+      address_count: address_count,
     });
   } catch (err) {
     console.error(err);
