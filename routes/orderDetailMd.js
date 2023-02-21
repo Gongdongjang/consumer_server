399fb3a37f60f8d4c1e7c2b1cf0a127aaaab8cff
leejw-lu <jiwoojenny@naver.com>
2023-02-21 16:31:54 +0900

    [기능추가] 주문상세페이지 수정

--- routes/orderDetailMd.js
+++ routes/orderDetailMd.js
@@ -10,10 +10,22 @@ router.post("/", async (req, res, next) => {
   try {
     //문제 없으면 try문 실행
     let [pu_date] = await pool.execute(
-      `SELECT order_pu_date FROM ggdjang.order WHERE order_id = ${order_id}`
+      `SELECT order_pu_time FROM ggdjang.order WHERE order_id = ${order_id}`
     );
 
-    pu_date = new Date(pu_date[0].order_pu_date).toLocaleDateString();
+    const [order_detail] = await pool.execute(
+      `SELECT md_id, order_pu_time, order_md_status, order_select_qty, order_price FROM ggdjang.order WHERE order_id = ${order_id}`
+    );
+
+    let [md_status] = await pool.execute(
+      `SELECT stk_confirm FROM ggdjang.stock WHERE md_id = ${order_detail[0].md_id}`
+    );
+
+    // console.log(order_detail); // 이거쓰기 pu_date도
+    // console.log(md_status[0]);
+
+    pu_date = new Date(pu_date[0].order_pu_time).toLocaleDateString();
+    //console.log(pu_date);
 
     resultCode = 200;
     message = "orderDetailMd 성공";
@@ -22,7 +34,10 @@ router.post("/", async (req, res, next) => {
       code: resultCode,
       message: message,
       pu_date: pu_date,
+      order_detail: order_detail,
+      md_status: md_status[0]
     });
+
   } catch (err) {
     console.error(err);
     return res.status(500).json(err);
--- routes/orderInsert.js
+++ routes/orderInsert.js
@@ -14,12 +14,13 @@ router.post("/", async (req, res, next) => {
   const order_pu_date= new Date(pu_date);   //문자열을 날짜형으로 변환
   const order_date=new Date();
   const order_pu_time=new Date(pu_date+" "+pu_time+":00");
+
   //console.log(order_date,order_pu_date,order_pu_time);
 
   try {
     //문제 없으면 try문 실행
     const [order_insert] = await pool.execute(
-      `INSERT INTO ggdjang.order (order_select_qty, order_pu_date, order_date, order_md_status, order_pu_time, order_price, user_id, md_id, store_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, 
+      `INSERT INTO ggdjang.order (order_select_qty, order_pu_date, order_date, order_md_status, order_pu_time, order_price, user_id, md_id, store_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
       [select_qty,order_pu_date,order_date,"준비중",order_pu_time,order_price,user_id,md_id,store_id]
     );
     resultCode = 200;
@@ -31,9 +32,15 @@ router.post("/", async (req, res, next) => {
     const stock_total= await pool.execute(`UPDATE stock SET stk_total=stk_total+${select_qty} WHERE md_id=${md_id}`);
     // stk_total = select sum(order_select_qty) from `order` join md on md.md_id = `order`.md_id where md.md_id = 5;
 
+    //order_id 보내기
+    const [order_id] = await pool.execute(
+      `SELECT LAST_INSERT_ID() FROM ggdjang.order WHERE user_id = ${user_id}`
+    );
+
     return res.json({
       code: resultCode,
       message: message,
+      order_id: order_id[0]
     });
   } catch (err) {
     console.error(err);
