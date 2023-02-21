fb0eb09567affe33008ae14045515a4d06154fad
leejw-lu <jiwoojenny@naver.com>
2023-02-21 22:59:00 +0900

    [기능추가] 상품 현재 상태 보이기 및 주석 정리

--- routes/orderDetailMd.js
+++ routes/orderDetailMd.js
@@ -9,23 +9,14 @@ router.post("/", async (req, res, next) => {
 
   try {
     //문제 없으면 try문 실행
-    let [pu_date] = await pool.execute(
-      `SELECT order_pu_time FROM ggdjang.order WHERE order_id = ${order_id}`
-    );
-
     const [order_detail] = await pool.execute(
       `SELECT md_id, order_pu_time, order_md_status, order_select_qty, order_price FROM ggdjang.order WHERE order_id = ${order_id}`
     );
 
+    //상품 현재 상태
     let [md_status] = await pool.execute(
       `SELECT stk_confirm FROM ggdjang.stock WHERE md_id = ${order_detail[0].md_id}`
     );
-
-    // console.log(order_detail); // 이거쓰기 pu_date도
-    // console.log(md_status[0]);
-
-    pu_date = new Date(pu_date[0].order_pu_time).toLocaleDateString();
-    //console.log(pu_date);
 
     resultCode = 200;
     message = "orderDetailMd 성공";
@@ -33,7 +24,6 @@ router.post("/", async (req, res, next) => {
     return res.json({
       code: resultCode,
       message: message,
-      pu_date: pu_date,
       order_detail: order_detail,
       md_status: md_status[0]
     });
