4f20fc3e6c7e7b8b66567f5fe62d85f3e64d242f
plum-king <jeesoo0908@gmail.com>
2023-03-01 04:24:05 +0900

    #28 [기타] 사진 전달 API 수정

--- routes/storeDetail.js
+++ routes/storeDetail.js
@@ -24,7 +24,7 @@ router.post("/storeDetail", async (req, res, next) => {
 
     //스토어 상세정보
     const [store_result] = await pool.execute(
-      `SELECT store_name, store_info, store_loc, store_phone FROM store WHERE store_id= ${store_id}`
+      `SELECT * FROM store WHERE store_id= ${store_id}`
     );
 
     //스토어에 있는 제품리뷰
@@ -34,7 +34,7 @@ router.post("/storeDetail", async (req, res, next) => {
 
     //스토어 현재 진행중인 공동구매
     const [md_data] = await pool.execute(
-      `SELECT * FROM md join payment on md.md_id = payment.md_id join pickup on md.md_id = pickup.md_id join store on pickup.store_id = store.store_id join farm on md.farm_id=farm.farm_id where pickup.store_id =${store_id}`
+      `SELECT * FROM md join payment on md.md_id = payment.md_id join pickup on md.md_id = pickup.md_id join md_Img on md.md_id = md_Img.md_id join store on pickup.store_id = store.store_id join farm on md.farm_id=farm.farm_id where pickup.store_id =${store_id}`
     );
 
     //스토어 운영 시간
