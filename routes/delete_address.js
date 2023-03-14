a5cec5fe537a24a361c590a922f9c7cdba88b70b
leejw-lu <jiwoojenny@naver.com>
2023-03-14 12:52:02 +0900

    #45 [기능추가] 최초 회원가입 시 주소삭제 막기

--- routes/delete_address.js
+++ routes/delete_address.js
@@ -11,6 +11,7 @@ router.post("/", async (req, res, next) => {
 
   let resultCode = 404;
   let message = "에러가 발생했습니다.";
+  let first_time="yes";
 
   const u_data = await pool.query("SELECT user_no FROM user WHERE user_id=? ", [userid]);
   const userno=u_data[0][0].user_no;
@@ -21,42 +22,54 @@ router.post("/", async (req, res, next) => {
   let sql2=`UPDATE address_user SET loc2= null WHERE userno = ?` ;
   let sql3=`UPDATE address_user SET loc3= null WHERE userno = ?` ; 
 
+  if(address[0]==undefined)//처음 회원가입
+  first_time="yes" ;
+  else first_time="no" ;
+
   try {
 
+    //처음 주소 등록이면 주소 삭제 안되게
+    if(first_time=="yes"){
+      message= "처음 주소 등록시에는 주소 삭제 불가합니다.";
+    } else {
+      
     if(number=="1") {
-        if(address[0].standard_address==address[0].loc1){
-            message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
-        }else{
-            const data = await pool.query(sql1, [userno]);
-            message = "주소삭제완료";
-            if(address[0].loc2 !=null && address[0].loc !=null ){ //주소2,3 있으면 주소1,2로 옮기기
-                const move = await pool.query(`UPDATE address_user SET loc1=?, loc2=?, loc3=null WHERE userno = ?`, [address[0].loc2, address[0].loc3, userno]);
-            } else {
-                const move = await pool.query(`UPDATE address_user SET loc1=?, loc2=null WHERE userno = ?`, [address[0].loc2, userno]);
-            }
-        }
+      if(address[0].standard_address==address[0].loc1){
+          message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
+      }else{
+          const data = await pool.query(sql1, [userno]);
+          message = "주소삭제완료";
+          if(address[0].loc2 !=null && address[0].loc !=null ){ //주소2,3 있으면 주소1,2로 옮기기
+              const move = await pool.query(`UPDATE address_user SET loc1=?, loc2=?, loc3=null WHERE userno = ?`, [address[0].loc2, address[0].loc3, userno]);
+          } else {
+              const move = await pool.query(`UPDATE address_user SET loc1=?, loc2=null WHERE userno = ?`, [address[0].loc2, userno]);
+          }
       }
-      //주소2
-      else if (number=="2") {
-        if(address[0].standard_address==address[0].loc2){
-            message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
-        }else{
-            const data = await pool.query(sql2, [userno]);
-            message = "주소삭제완료";
-            if(address[0].loc3 !=null){ //주소3이 있으면 주소2 위치로 옮기기
-                const move = await pool.query(`UPDATE address_user SET loc2=?, loc3=null WHERE userno = ?`, [address[0].loc3, userno]);
-            }   
-        }
+    }
+    //주소2
+    else if (number=="2") {
+      if(address[0].standard_address==address[0].loc2){
+          message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
+      }else{
+          const data = await pool.query(sql2, [userno]);
+          message = "주소삭제완료";
+          if(address[0].loc3 !=null){ //주소3이 있으면 주소2 위치로 옮기기
+              const move = await pool.query(`UPDATE address_user SET loc2=?, loc3=null WHERE userno = ?`, [address[0].loc3, userno]);
+          }   
       }
-      //주소3
-      else { //number=="3" 이면 안떙겨도 됨.
-        if(address[0].standard_address==address[0].loc3){
-               message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
-         }else{
-              const data = await pool.query(sql3, [userno]);
-              message = "주소삭제완료";
-        }
-        }  
+    }
+    //주소3
+    else { //number=="3" 이면 안떙겨도 됨.
+      if(address[0].standard_address==address[0].loc3){
+             message= "기준주소지로 설정되어 주소 삭제 불가합니다.";
+       }else{
+            const data = await pool.query(sql3, [userno]);
+            message = "주소삭제완료";
+      }
+    }  
+    }
+
+
 
 
     resultCode = 200;
